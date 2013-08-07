ee0618bde2bea52bf7261793a55f58a11da630f2
Carolina Caceres <carolina.caceres@unholster.com>
2013-08-07 14:29:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a36311d1ddd7df02ed79faa99a915acf0516a06009252361bb2c7c3bedb04a649cce41271310b673fdf8f17bad381b4447d08636484b4d243afd5fc7b10b3463'
